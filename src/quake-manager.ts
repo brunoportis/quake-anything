a5704af53b0043f52165f824cee1e3c782d870ff
Bruno Portis <bruno.sandivilli@gmail.com>
2026-09-19 12:16:35 -0300

    chore: remove top crop diagnostics

--- src/quake-manager.ts
+++ src/quake-manager.ts
@@ -624,23 +624,6 @@ export class QuakeManager {
         actor.set_size(bufferRect.width, bufferRect.height);
         this._applyTopCrop(actor, topCrop, bufferRect.width, bufferRect.height);
         actor.set_translation(0, -topCrop, 0);
-
-        console.log('[quake-crop] applied', JSON.stringify({
-            entryId,
-            windowId: win.get_id(),
-            topCrop,
-            bufferRect: {
-                x: bufferRect.x,
-                y: bufferRect.y,
-                width: bufferRect.width,
-                height: bufferRect.height,
-            },
-            actorX: actor.get_x(),
-            actorY: actor.get_y(),
-            actorWidth: actor.get_width(),
-            actorHeight: actor.get_height(),
-            translationY: -topCrop,
-        }));
     }
 
     private _rememberQuakePercent(entryId: string, win: Meta.Window, entry: QuakeEntry): void {
@@ -792,20 +775,6 @@ export class QuakeManager {
         this._applyTopCrop(actor, topCrop, bufferRect.width, bufferRect.height);
         actor.set_translation(offset.x, offset.y - topCrop, 0);
         actor.set_opacity(255);
-
-        if (topCrop > 0) {
-            console.log('[quake-crop] show', JSON.stringify({
-                entryId,
-                windowId: win.get_id(),
-                topCrop,
-                bufferRect: {
-                    x: bufferRect.x,
-                    y: bufferRect.y,
-                    width: bufferRect.width,
-                    height: bufferRect.height,
-                },
-            }));
-        }
         win.activate(global.get_current_time());
 
         this._animating.add(entryId);
